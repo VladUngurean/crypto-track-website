6bf64e6a42763e92eb74ba7de6db32804f5bfab4
Vlad Ungurean <ghiena14@gmail.com>
2023-09-10 18:14:07 +0300

    code cleanup

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -4,10 +4,10 @@ export default function NavBar() {
   return (
     <nav className="flex justify-center p-4 mx-11 max-sm:mx-2">
       <div className="w-full max-w-[1000px] flex justify-between max-md:flex-col max-md:justify-center max-md:items-center">
-        <div className="mb-2">
+        <div className="">
           <p className="text-2xl font-bold text-white">COINTRACK</p>
         </div>
-        <div className="">
+        <div className="flex justify-center items-center">
           <a href="#" className="nav_links ml-0">
             Home
           </a>
@@ -21,7 +21,7 @@ export default function NavBar() {
             Join
           </a>
         </div>
-        <div className="mt-2 w-[70px] flex justify-between">
+        <div className="mt-0 w-[70px] flex justify-between">
           <FaTwitter className=" w-6 h-full text-white" />
           <FaDiscord className=" w-6 h-full text-white" />
         </div>
