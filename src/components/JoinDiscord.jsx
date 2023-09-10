d4c915905e403d43a6b4328323e094c6c7830419
Vlad Ungurean <ghiena14@gmail.com>
2023-09-10 18:04:31 +0300

    footer, join discord responsive end

--- src/components/JoinDiscord.jsx
+++ src/components/JoinDiscord.jsx
@@ -10,7 +10,7 @@ const JoinDiscord = () => {
             src="icons/bitcoin.png"
             alt="BTC"
           />
-          <p className="text-center text-white text-6xl px-4 font-bold max-lg:text-5xl">
+          <p className="text-center text-white text-6xl px-4 font-bold max-sm:text-4xl max-lg:text-5xl">
             JOIN US VIA
           </p>
           <img
@@ -20,14 +20,14 @@ const JoinDiscord = () => {
           />
         </div>
         <div className=" max-w-screen-lg mx-auto flex items-center justify-center">
-          <p className="text-center text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#2100fc] to-[#ff00ff] max-lg:text-5xl">
+          <p className="text-center text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#2100fc] to-[#ff00ff] max-sm:text-4xl max-lg:text-5xl">
             DISCORD
           </p>
         </div>
         <p className=" text-white my-10">
           Invest and manage all your crypto at one place.
         </p>
-        <button className=" font-extrabold bg-gradient-to-t from-[#2100fc] to-[#ff00ff] rounded-3xl px-7 py-4 text-white">
+        <button className="font-extrabold bg-gradient-to-t from-[#2100fc] to-[#ff00ff] rounded-3xl px-7 py-4 text-white">
           Join Via Discord
         </button>
       </div>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
   return (
     <>
       <div className="flex flex-row justify-center items-baseline relative">
-        <div className="w-[300px] flex justify-start absolute bottom-0 left-0">
+        <div className="w-[300px] flex justify-start absolute z-0 bottom-0 left-0">
           <img className="max-w-[300px]" src="images/chest.png" alt="chest" />
         </div>
-        <div className="text-white w-[200px] flex flex-row justify-between">
+        <div className="text-white max-sm:text-yellow-400 w-[200px] flex flex-row justify-between relative z-10">
           <span className="h-[60px]">
             <FaTwitter className="w-[50px] h-[30px]" />
           </span>
@@ -22,7 +22,7 @@ const Footer = () => {
             <FaYoutube className="w-[50px] h-[30px]" />
           </span>
         </div>
-        <div className="w-[300px] flex justify-end absolute bottom-0 right-0">
+        <div className="w-[300px] flex justify-end absolute z-0 bottom-0 right-0">
           <img className="max-w-[150px]" src="images/coinImg.png" alt="coin" />
         </div>
       </div>
